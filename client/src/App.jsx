d43020a5dbf2ef6b3b788009078aa76e2de7fc74
oskarali <alos24dh@student.ju.se>
2026-09-24 22:17:35 +0200

    added login page and clickable login button in the navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { CssBaseline } from '@mui/material'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
 
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
         </Routes>
     </BrowserRouter>
 
